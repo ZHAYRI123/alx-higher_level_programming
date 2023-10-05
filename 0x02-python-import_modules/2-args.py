2c81a3bb6068b2875e220c836ede95e13cb4ee3d
ZHAYRI123 <merykozan@gmail.com>
2023-10-05 14:03:39 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print(":")
     for i in range(m):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
