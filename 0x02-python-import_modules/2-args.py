bb68dfe8966c159bf9e7a479cdc58442f0587ddf
ZHAYRI123 <merykozan@gmail.com>
2023-10-05 13:34:58 -0700

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    m = len(sys.argv) - 1
+    print("{} argument".format(m), end='')
+    if m == 0:
+        print("s.")
+    else:
+        print(":")
+        for i in range(m):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
