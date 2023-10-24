431655593aea6ac3dc309fa0d443a4ddd21f9aa3
ZHAYRI123 <merykozan@gmail.com>
2023-10-24 13:55:21 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+class Square:
+    """Represent square"""
+
+    def __init__(self, size):
+        """Initialisation this square class
+        Args: size - present the size of the square"""
+
+        self.__size = size
