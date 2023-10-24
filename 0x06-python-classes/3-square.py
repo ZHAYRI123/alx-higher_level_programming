a2d4b471e684199aaecbe3cfc299cf88bbf5778e
ZHAYRI123 <merykozan@gmail.com>
2023-10-24 14:18:04 -0700

    update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a Class"""
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """initializing a square size
+        Args: size - square size
+        raises: TypeError or Valueerror"""
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        self.__size = size
+    def area(self):
+        """ returns the current square area"""
+
+        return (self.__size ** 2)
