f738bd548fc45b766f45531b08effb3999ef1cd0
ZHAYRI123 <merykozan@gmail.com>
2023-11-06 10:58:57 -0800

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the lookup function
+"""
+
+
+def lookup(obj):
+    """ a function that returns the list of available attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+You can define a class in Python that inherits from another class by placing the name of the parent class in parentheses immediately after the name of the child class
