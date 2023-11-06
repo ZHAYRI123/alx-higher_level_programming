c849f99b5554156259c120eca1fcb4184b8d6a90
ZHAYRI123 <merykozan@gmail.com>
2023-11-06 11:24:22 -0800

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,7 +9,7 @@ class MyList(list):
 
     def __init__(self):
         """initializes the object"""
-        uper().__init__()
+        super().__init__()
 
     def print_sorted(self):
         """prints the sorted list"""
