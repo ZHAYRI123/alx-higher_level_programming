ecddb46eef03ed52edf0c8545f59b2d16ec4d83c
ZHAYRI123 <merykozan@gmail.com>
2023-11-06 11:18:04 -0800

    update

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains print_sorted function
+"""
+
+
+class MyList(list):
+    """ a class MyList that inherits from list"""
+
+    def __init__(self):
+        """initializes the object"""
+        uper().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
+
