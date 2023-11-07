d3bb557261cd1d49ac2365671fd2c96c5761b407
ZHAYRI123 <merykozan@gmail.com>
2023-11-07 12:28:24 -0800

    updte

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,5 @@ def write_file(filename="", text=""):
     """writes a string to a text file"""
     with open(filename, 'w', encoding='UTF8') as file:
         file.write(text)
-        count = len(text)
+    count = len(filename)
     return (count)
