850f6b1520505a3f743d1b149b74482b95a0cb4c
ZHAYRI123 <merykozan@gmail.com>
2023-11-07 13:14:15 -0800

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+has write fct
+"""
+
+
 def write_file(filename="", text=""):
     """writes a string to a text file"""
     with open(filename, 'w', encoding='UTF8') as file:
-        file.write(text)
-    count = len(filename)
-    return (count)
+        return (file.write(text))
