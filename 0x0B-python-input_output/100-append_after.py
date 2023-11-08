344ed2de534c083a8b04df2c391fe069fa8260a5
ZHAYRI123 <merykozan@gmail.com>
2023-11-08 14:34:42 -0800

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,7 +8,7 @@ def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file"""
     with open(filename, 'r', encoding='UTF8') as file:
         l_list = []
-        while True:
+        while filename:
             li = file.readline()
             if li == "":
                 break
