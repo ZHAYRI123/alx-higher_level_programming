c69d3d9dad726f7d8dccf79052b4e97c1ef3412b
ZHAYRI123 <merykozan@gmail.com>
2023-11-08 14:30:18 -0800

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,9 +4,6 @@ has append_after fct
 """
 
 
-import json
-
-
 def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file"""
     with open(filename, 'r', encoding='UTF8') as file:
