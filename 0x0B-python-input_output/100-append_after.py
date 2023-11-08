00cb170a8420c0cbf205a8362ffc97ce25160d32
ZHAYRI123 <merykozan@gmail.com>
2023-11-08 14:39:04 -0800

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,13 +7,13 @@ has append_after fct
 def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file"""
     with open(filename, 'r', encoding='UTF8') as file:
-        l_list = []
+        l_list = ""
         while filename:
             li = file.readline()
             if li == "":
                 break
-            l_list.append(li)
+            l_list += li
             if search_string in li:
-                l_list.append(new_string)
+                l_list += (new_string)
     with open(filename, 'w', encoding='UTF8') as file:
-        file.writelines(line_list)
+        file.writelines(l_list)
