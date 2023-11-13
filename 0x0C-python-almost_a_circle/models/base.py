375200c35242be296942538d2c80d001ad31c09f
ZHAYRI123 <merykozan@gmail.com>
2023-11-13 09:42:14 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation"""
+        m = []
+        if (list_dictionaries):
+            m = json.dumps(list_dictionaries)
+        return (m)
