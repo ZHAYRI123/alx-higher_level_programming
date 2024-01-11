a47e44da8687f36239fd591f8a2bf5e46d4c3474
ZHAYRI123 <merykozan@gmail.com>
2024-01-11 13:18:50 -0800

    update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const arg =parseInt(process.argv[2]);
+if (isNaN(arg))
+{
+	console.log("Missing size");
+} else if (arg > 0)
+{
+	let i = 0;
+	let j = 0;
+	let li = '';
+	while (i < arg)
+	{
+		while (j < arg)
+		{
+			li = li + 'X';
+			j++;
+		}
+		console.log("li");
+		i++;
+	}}
