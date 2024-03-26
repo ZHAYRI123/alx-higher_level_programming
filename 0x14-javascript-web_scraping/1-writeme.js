ac07441a05787a3189c8a3f1bcb4d7fd678802c6
ZHAYRI123 <merykozan@gmail.com>
2024-03-26 23:58:33 +0000

    fix task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-function writeToFile(filePath, content) {
+function writeToFileSync(filePath, content) {
   try {
-    fs.writeFile(filePath, content, 'utf-8');
+    fs.writeFileSync(filePath, content, 'utf-8');
   } catch (err) {
     console.error(err);
   }
 }
-
 if (process.argv.length !== 4) {
   console.log('Usage: ./1-writeme.js <file_path> <content>');
   process.exit(1);
 }
-
 const filePath = process.argv[2];
 const content = process.argv[3];
+writeToFileSync(filePath, content);
 
-writeToFile(filePath, content);
